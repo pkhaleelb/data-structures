35f0da6204de1f69d114f05f60dc1766358d0399
pkhaleelb <pkbasha143@gmail.com>
2024-12-26 21:55:33 +0530

    Create list cls.py

---
+++ list cls.py
@@ -0,0 +1,43 @@
+# a=[10,'ranma',99,99.99]
+# print(a)
+
+# quiz application fot list
+
+# q=('1. what is the capital of india:?',
+#    '2. what is the national animal:?')
+# ans=('delhi','tiger')
+# for x in q:
+#     print(x)
+#     user_ans=(input('enter answer:').lower())
+#     if user_ans in ans:
+#         print("correct ans")
+#     else:
+#         print("wrogn ans")
+
+# adding length and range to get the correct answer given question
+
+# q=('1. what is the capital of india?',
+#    '2. what is the national animal?')
+# ans=('delhi','tiger')
+# for x in range(len(q)):
+#     print(q[x])
+#     user_ans=input('enter answer:').lower()
+#     if user_ans==ans[x]:
+#         print('correct ans')
+#     else:
+#         print('wrogn ans')
+
+#indexing
+
+# val=('a',10,100)
+# print(val[1])
+
+# nested list
+
+# sales=(100,200,(60,70))
+# print(sales[2][1])
+
+# q.m
+
+# att=(1,44,66,88,[66,77],90)
+# print(att[4][0])
